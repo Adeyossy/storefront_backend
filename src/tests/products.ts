c8ae4682a6e4554ab5335374a528e28da844d34f
Adeyossy <adeyossy1@gmail.com>
2023-02-13 18:17:26 +0100

    feat: added testing for products route and models

--- src/tests/products.ts
+++ src/tests/products.ts
@@ -1,11 +1,39 @@
 import app from '../server';
 import supertest from 'supertest';
+import { ModelDB } from '../models/model';
+import { Product } from '../models/types';
 
 const request = supertest(app);
 
-describe('This suite tests the api endpoints for products routes', function() {
-  it('and this spec tests the /products GET route', async function() {
-    const response = request.get('/products');
-    expect(response)
+describe('This suite tests the api endpoints for products routes', function () {
+  it('and this spec tests the /products GET route', async function () {
+    const response = await request.get('/products');
+    console.log(response.body);
+    expect(response.status).toBe(200);
+  });
+});
+
+describe('Suite for testing products models', function () {
+  const productsModel = new ModelDB('products');
+
+  it('beginning with a CREATE action spec', async function () {
+    const newProduct = <Product[]> await productsModel.createEntity({
+      name: 'Sandwich', 
+      price: 300, 
+      category: 'Food'
+    });
+
+    expect(Array.isArray(newProduct)).toBeTrue();
+    console.log(typeof newProduct[0]);
+    expect(typeof newProduct[0]).toBe('Product');
+    expect(newProduct[0].name).toBe('Sandwich');
+  });
+
+  it('then SHOW method spec', async function() {
+    const allProducts = <Product[]> await productsModel.getIndex();
+    expect(Array.isArray(allProducts)).toBeTrue();
+    if (allProducts.length) {
+      expect(allProducts[0].hasOwnProperty('name')).toBeTrue();
+    }
   })
 })
